27428237e10095745c3b863cf7152fedb532e61a
VINICIUS CANDIDO DOS SANTOS <vinicius.csantos15@senacsp.edu.br>
2018-04-19 11:11:11 -0300

    Declaração de variaeis

--- OperacoesCondicionais/Program.cs
+++ OperacoesCondicionais/Program.cs
@@ -6,7 +6,17 @@ namespace OperacoesCondicionais
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        //Comentario in-line(em linha)
+        /*
+        Comentario in-block(em bloco)
+         */
+         //declaração da variavel operação. Ela será dp tipo inteira
+         
+         int operaçao;
+          
+          //declaração das variaveis n1 e n2 elas serão do tipo double
+          double n1, n2;
+           
         }
     }
 }
